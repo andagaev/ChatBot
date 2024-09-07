f09fed8ac3a9ac721802ed962226e3877efb2df1
Andrei Agaev <andagaev@gmail.com>
2024-09-07 10:51:02 -0700

    implement reading from file
    - print file content
    - add ruff

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
+import sys
+
+
 def main():
+    file_path = get_file_path()
+    message = read_from_file(file_path)
+
+    print(message)
+
+
+def get_file_path() -> str:
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    else:
+        return "File path is not provided, please provide file path"
+
+
+def read_from_file(path: str) -> str:
+    try:
+        with open(path, "r") as f:
+            message = f.read()
+            return message
+    except FileNotFoundError as e:
+        return f"Error occured: {e}"
+
+
+def send_message_to_chat(message):
     pass
+
+
+if __name__ == "__main__":
+    main()
