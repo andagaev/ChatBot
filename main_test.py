b798b50bdbf8f3f95bbd29acb7e235e23a0451c8
Andrei Agaev <andagaev@gmail.com>
2024-09-07 21:56:24 -0700

    add test for read_from_file func
    - add pytest-mock
    - configure testing in settings.json for VS code

---
+++ main_test.py
@@ -0,0 +1,21 @@
+import main
+
+
+def test_read_from_file_success(mocker):
+    # Arrange
+    mock_open = mocker.mock_open(read_data="mocked content")
+    mocker.patch("builtins.open", mock_open)
+
+    # Act
+    result = main.read_from_file("./test.txt")
+
+    # Assert
+    assert result == "mocked content"
+
+
+def test_read_from_file_fail():
+    # Act
+    result = main.read_from_file("wrong path")
+
+    # Assert
+    assert result == "Error occured: [Errno 2] No such file or directory: 'wrong path'"
