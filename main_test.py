4fc13f22dd6c8b14c62dac023379fe127c6e7286
Andrei Agaev <andagaev@gmail.com>
2024-09-07 22:11:59 -0700

    add tests for get_file_path func

--- main_test.py
+++ main_test.py
@@ -1,4 +1,5 @@
 import main
+import sys
 
 
 def test_read_from_file_success(mocker):
@@ -19,3 +20,18 @@ def test_read_from_file_fail():
 
     # Assert
     assert result == "Error occured: [Errno 2] No such file or directory: 'wrong path'"
+
+
+def test_get_file_path_success(mocker):
+    passed_arg = "test_arg"
+    mocker.patch.object(sys, "argv", ["main.py", passed_arg])
+
+    result = main.get_file_path()
+    assert result == passed_arg
+
+
+def test_get_file_path_fail(mocker):
+    mocker.patch.object(sys, "argv", ["main.py"])
+
+    result = main.get_file_path()
+    assert result == "File path is not provided, please provide file path"
