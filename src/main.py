d2c7fa23ce00c9a1f0536dede00050f4848caf4c
Andrei Agaev <andagaev@gmail.com>
2024-09-24 12:35:52 -0700

    Add file_path to .env, remove def get_file_path()

--- src/main.py
+++ src/main.py
@@ -1,18 +1,13 @@
-import sys
+import os
 
 from src import tg
 
+file_path = os.environ["FILE_PATH"]
+
 
 def main():
-    file_path = get_file_path()
     message = read_from_file(file_path)
     tg.send_message_to_Telegram(message_to_send=message)
-
-
-def get_file_path() -> str:
-    if len(sys.argv) > 1:
-        return sys.argv[1]
-    return "File path is not provided, please provide file path"
 
 
 def read_from_file(path: str) -> str:
